4244d2b6bd6341f32fc9f97e9499f48abfb586e5
NameAr9 <m4rkl4rico18@gmail.com>
2022-06-26 22:22:33 -0500

    Lab15

---
+++ lab15/frontend/src/services/auth.services.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+const API_URL = "http://localhost:8000/"
+
+class AuthService{
+    login(usuario,password){
+        return axios
+        .post(API_URL + "login/",{
+            username : usuario,
+            password : password
+        }).then(res=>{
+            if(res.data.access){
+                console.log(JSON.stringify(res.data.content));
+                localStorage.setItem('token', JSON.stringify(res.data.access))
+            }
+            return res.data;
+        })
+    }
+    getToken(){
+        return JSON.parse(localStorage.getItem('token'))
+    }
+}
+export default new AuthService();
